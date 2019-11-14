baf3287cbd0e19cece23857d6012bb2ba04bce2b
Luciferclc <luciferclc@foxmail.com>
2019-11-14 11:45:36 +0800

    修改配置

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 const IS_DEV = process.env.NODE_ENV !== 'production';
 
-export const API_ROOT = IS_DEV ? 'http://localhost:8080/api/' : 'https://api.nolan.cc/api/';
+export const API_ROOT = IS_DEV ? 'http://localhost:8000/api/' : 'https://api.nolan.cc/api/';
